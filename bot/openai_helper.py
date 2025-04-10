14bf4305a5cfdbeed40b818d59500485cc48009f
Git-Mon555 <reahimmeno@gmail.com>
2025-04-10 21:08:07 +0900

    bot updated

--- bot/openai_helper.py
+++ bot/openai_helper.py
@@ -52,6 +52,8 @@ def default_max_tokens(model: str) -> int:
         return 4096
     elif model in GPT_4O_MODELS:
         return 4096
+    elif model in O_MODELS:
+        return 4096
 
 
 def are_functions_available(model: str) -> bool:
